6c5732d574e4bff4e67352e2113acfe54107ac4b
bennettscience <brian.bennett2@gmail.com>
2022-10-31 22:00:15 -0400

    work on inheritance

--- upstream/blueprints/home.py
+++ upstream/blueprints/home.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from htmx_flask import request
 
 from upstream.models import Event
 
@@ -8,4 +9,8 @@ bp = Blueprint("home", __name__)
 @bp.get("/")
 def index():
     events = Event.query.order_by(Event.starts.desc()).all()
-    return render_template("home/index.html", events=events)
+    if request.htmx:
+        template = "home/index-htmx.html"
+    else:
+        template = "home/index.html"
+    return render_template(template, events=events)

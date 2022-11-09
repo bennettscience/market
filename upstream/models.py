fe15e64fafec0748b8d5523b698d85531e1b3bda
bennettscience <brian.bennett2@gmail.com>
2022-11-08 20:52:02 -0500

    Add ID to transactions

--- upstream/models.py
+++ upstream/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.dialects.mysql import FLOAT
+from sqlalchemy.sql import func
 from upstream.extensions import db
 
 
@@ -6,8 +7,8 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
 
-    sales = db.relationship("Transaction", backref="sale", uselist=True)
-    event_inventory = db.relationship("EventItem", backref="item")
+    sales = db.relationship("Transaction", backref="item", lazy="dynamic")
+    events = db.relationship("EventItem", backref="item", lazy="dynamic")
 
 
 class Location(db.Model):
@@ -26,10 +27,11 @@ class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     starts = db.Column(db.DateTime, nullable=False)
-    created_at = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, default=func.now())
 
-    sales = db.relationship("Transaction", backref="event", uselist=True)
-
+    sales = db.relationship(
+        "Transaction", backref="sales", lazy="dynamic", uselist=True
+    )
     inventory = db.relationship("EventItem", backref="event", uselist=True)
 
 
@@ -48,13 +50,12 @@ class EventItem(db.Model):
 
 class Transaction(db.Model):
     __tablename__ = "transaction"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    occurred_at = db.Column(db.DateTime(timezone=True), default=func.now())
     event_id = db.Column(
         db.Integer,
         db.ForeignKey("event.id", onupdate="CASCADE", ondelete="CASCADE"),
-        primary_key=True,
     )
-    event_item_id = db.Column(
-        db.Integer, db.ForeignKey("item.id", onupdate="CASCADE"), primary_key=True
-    )
+    event_item_id = db.Column(db.Integer, db.ForeignKey("item.id", onupdate="CASCADE"))
     price_per_item = db.Column(FLOAT(precision=20, scale=10), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)

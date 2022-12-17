e09f858cce37817821752123aca58e122c53c1be
Brian Bennett <brian.bennett2@gmail.com>
2022-12-16 22:00:36 -0500

    Rebuild SQLite for unique username

--- migrations/versions/412446f3c449_.py
+++ migrations/versions/412446f3c449_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2b3e0911fc09
+Revision ID: 412446f3c449
 Revises: 
-Create Date: 2022-11-23 20:09:50.459918
+Create Date: 2022-12-16 21:48:14.097443
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision = '2b3e0911fc09'
+revision = '412446f3c449'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -40,7 +40,10 @@ def upgrade():
     op.create_table('user',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=50), nullable=True),
-    sa.PrimaryKeyConstraint('id')
+    sa.Column('username', sa.String(length=32), nullable=True),
+    sa.Column('password_hash', sa.String(length=128), nullable=True),
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('username')
     )
     op.create_table('event_item',
     sa.Column('event_id', sa.Integer(), nullable=False),

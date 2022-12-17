aab4b8a962a269e084362ad3887ddc4d9a571873
Brian Bennett <brian.bennett2@gmail.com>
2022-12-16 22:00:52 -0500

    Add gunicorn as app server

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+import logging
+from upstream import create_app
+
+app = create_app()
+
+# logs to the local logger.
+if __name__ != '__main__':
+	gunicorn_logger = logging.getLogger('gunicorn.error')
+	app.logger.handlers = gunicorn_logger.handlers
+	app.logger.setLevel(gunicorn_logger.level)
+
+# Handle running the script locally
+if __name__ == '__main__':
+	gunicorn_logger = logging.getLogger('gunicorn.error')
+	app.logger.handlers = gunicorn_logger.handlers
+	app.logger.setLevel(gunicorn_logger.level)
+	app.run()
